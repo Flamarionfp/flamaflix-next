13c0612d9aef1bfba49e811dc31e040b12cf7dd9
Flamarion Fagundes <flamarion.fagundes@lighthouseit.com.br>
2023-09-05 10:38:17 -0300

    test: add snapshot to avatar test

--- src/components/avatar/avatar.test.tsx
+++ src/components/avatar/avatar.test.tsx
@@ -2,13 +2,21 @@ import { render, screen } from "@testing-library/react";
 import { Avatar } from "./index";
 
 describe("Avatar Test Case", () => {
+  const avatarProps = {
+    src: "/path",
+    alt: "avatar",
+  };
+
   it("should be full rounded", async () => {
-    const altText = "avatar";
+    render(<Avatar {...avatarProps} />);
 
-    render(<Avatar src="/path" alt={altText} />);
-
-    const imageElement = await screen.findByAltText(altText);
+    const imageElement = await screen.findByAltText(avatarProps.alt);
 
     expect(imageElement).toHaveClass("rounded-full");
+  });
+
+  it("renders avatar unchanged", () => {
+    const { container } = render(<Avatar {...avatarProps} />);
+    expect(container).toMatchSnapshot();
   });
 });
